d6619acb95a827ee8bc2933f050310887a8ec292
Doragonkimu <furumeyu918@gmail.com>
2025-07-14 13:57:49 +0900

    11finish

--- pages/posts/nextjs-blog/pages/index.js
+++ pages/posts/nextjs-blog/pages/index.js
@@ -1,6 +1,6 @@
+import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Link from 'next/link';
 
 export default function Home() {
   return (
